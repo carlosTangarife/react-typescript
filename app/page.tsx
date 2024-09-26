cd27d4b04ab9bd5f23082c4e9bfd335768cc4b70
carlosTangarife <cjt860826@gmail.com>
2024-09-25 22:15:42 -0500

    refactor: enhance image handling with unique IDs for RandomFox components
    - Updated the `images` state in `Home` component to store objects with unique `id` and `url` properties
    - Replaced simple image URL array with an array of `ImageItem` objects to improve key handling in the map function
    - Wrapped each `RandomFox` component with a styled div to add padding and shadow effects
    - Added helper function `generedateId` to generate unique IDs for each image item

--- app/page.tsx
+++ app/page.tsx
@@ -2,22 +2,27 @@
 import { RandomFox } from "./components/RandomFox";
 import { useState } from "react";
 
+type ImageItem = { id: string, url: string };
+
 export default function Home() {
-  const [images, setImages] = useState<string[]>([
-    `https://randomfox.ca/images/${randomNumber()}.jpg`,
-    `https://randomfox.ca/images/${randomNumber()}.jpg`,
-    `https://randomfox.ca/images/${randomNumber()}.jpg`,
-    `https://randomfox.ca/images/${randomNumber()}.jpg`,
+  const [images, setImages] = useState<Array<ImageItem>>([
+    { id: generedateId(), url: `https://randomfox.ca/images/${randomNumber()}.jpg` },
+    { id: generedateId(), url: `https://randomfox.ca/images/${randomNumber()}.jpg` },
+    { id: generedateId(), url: `https://randomfox.ca/images/${randomNumber()}.jpg` },
+    { id: generedateId(), url: `https://randomfox.ca/images/${randomNumber()}.jpg` },
+    { id: generedateId(), url: `https://randomfox.ca/images/${randomNumber()}.jpg` },
   ]);
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1 className="text-3xl font-bold underline">Hello, Platzi</h1>
-        {images.map((image, index)=> <RandomFox key={index} imageUrl={image} />)}
+        {images.map(image => <div className="p-4 bg-white rounded-lg shadow-lg" key={image.id}> <RandomFox imageUrl={image.url} /> </div>)}
       </main>
     </div>
   );
 }
 
-const randomNumber = ()=> Math.floor(Math.random() * 123) + 1;
+const randomNumber = () => Math.floor(Math.random() * 123) + 1;
+
+const generedateId = () => Math.random().toString(36).substring(2, 9);
